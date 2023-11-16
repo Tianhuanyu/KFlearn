38d6b2255956c00afcc5d18d498ca91ecb2c0021
thy <HyTian>
2023-11-16 18:16:57 +0000

    change cost function no others

--- pipeline.py
+++ pipeline.py
@@ -96,10 +96,10 @@ class Pipeline:
                 # print("error = {0}".format(torch.norm(gt[:,0:3,:]-state[:,0:3,:])))
 
     def loss_with_acc(self,out,out_p,out_p2, y,x):
-        return 0.5*(self.criterion(out[:,0:3], y[:,0:3]) + 
-                             self.criterion(out[:,3:7], y[:,3:7])) + 0.1*(
+        return 1.0*(self.criterion(out[:,0:3], y[:,0:3]) + 
+                             self.criterion(out[:,3:7], y[:,3:7])) + 0.0*(
                                 self.criterion(out[:,0:3]+out_p2[:,0:3], 2.0*out_p[:,0:3]) + 
-                             self.criterion(out[:,3:7]+out_p2[:,3:7], 2.0*out_p[:,3:7]))+ 0.4*(
+                             self.criterion(out[:,3:7]+out_p2[:,3:7], 2.0*out_p[:,3:7]))+ 0.0*(
                                 self.criterion(x[:,0:3], out[:,0:3]) +  self.criterion(x[:,3:7], out[:,3:7])
                             )
 
